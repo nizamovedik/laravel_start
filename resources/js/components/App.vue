166bf491f62953f6432af41acca14923ea760760
Eduard Nizamov <nizamov.e@openregion.info>
2026-04-23 19:23:21 +0500

    Init Vue + Auth

---
+++ resources/js/components/App.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Привет из Vue 3 и Laravel!</h1>
+    <PostComponent></PostComponent>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/about', [AboutController::class, 'index'])->name('about.index');
 
 // Posts route
 Route::get('/posts', [PostController::class, 'index'])->name('post.index');
-Route::get('/posts/create', [PostController::class, 'create'])->name('post.create');
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth')->name('post.create');
 Route::post('/posts', [PostController::class, 'store'])->name('post.store');
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('post.show');
 Route::patch('/posts/{post}', [PostController::class, 'update'])->name('post.update');
@@ -23,3 +23,7 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.d
 Route::group(['namespace' => 'App\Http\Controllers\News'], function () {
     Route::get('/news', 'IndexController')->name('news.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
